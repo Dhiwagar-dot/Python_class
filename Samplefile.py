fb34a319942b8b60012a29e56b023921e6f524a3
Dhiwagar-dot <you@example.com>
2026-03-04 19:23:58 +0530

    Add Python file

---
+++ Samplefile.py
@@ -0,0 +1,13 @@
+print("hello world")
+Username = "Dhiwagar"
+phone_no = 8903681665
+is_active = True
+value =  6.41
+print(Username)
+print(phone_no)
+print(is_active)
+print(value)
+print(type(Username))
+print(type(phone_no))
+print(type(is_active))
+print(type(value))
