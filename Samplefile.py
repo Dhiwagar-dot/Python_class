a1ed8ee0f89aaf32d7cf24e6081978673f0f83ef
Dhiwagar-dot <you@example.com>
2026-03-06 18:21:55 +0530

    initial commit

--- Samplefile.py
+++ Samplefile.py
@@ -3,11 +3,23 @@ Username = "Dhiwagar"
 phone_no = 8903681665
 is_active = True
 value =  6.41
-print(Username)
-print(phone_no)
-print(is_active)
-print(value)
-print(type(Username))
-print(type(phone_no))
-print(type(is_active))
-print(type(value))
+# print(Username)
+# print(phone_no)
+# print(is_active)
+# print(value)
+# print(type(Username))
+# print(type(phone_no))
+# print(type(is_active))
+# print(type(value))
+# NameInput = input("Enter the your name: ")
+# print(NameInput)
+a = int(input("Enter the 'a' value: "))
+b = int(input("ENter the 'b' value: "))
+# print(type(a))
+# print(a + b)
+# print(a - b)
+if (a != b):
+    print("A and B are equal")
+    print(a*b)
+else:
+    print("A and B are not equal")
