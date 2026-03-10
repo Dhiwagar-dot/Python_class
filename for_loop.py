6253420db396a3e5f5f823be87762aa306158893
Dhiwagar-dot <you@example.com>
2026-03-10 18:30:59 +0530

    initial_commit

--- for_loop.py
+++ for_loop.py
@@ -1,4 +1,18 @@
 for i in range(5):
     print(i)
-for i in range(1, 6):
+
+for i in range(1, 10, 2):
+    print(i)
+
+for i in range(10):
+    if i == 5:
+        break
+    print("First loop:", i)
+
+for i  in range(10):
+    if i == 5:
+        continue
+    print("First loop:", i)
+    
+for i in range(0, 10, 2):
     print(i)
