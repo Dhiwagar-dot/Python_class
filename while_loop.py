6a0322df319c3980da6645d806cb68837a987803
Dhiwagar-dot <you@example.com>
2026-03-17 18:26:58 +0530

    initial commit

--- while_loop.py
+++ while_loop.py
@@ -1,5 +1,14 @@
 count = 1
 
-while count <=1:
+while count <=5:
     print(count)
     count+=1
+print("While loop is completed")
+
+n=1
+
+while n<=10:
+    n+=1
+    if n == 3: 
+        break
+    print(n)
