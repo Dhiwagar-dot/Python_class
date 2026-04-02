b837b2323e0a56bef5b9d84d27401756ab8ff476
Dhiwagar-dot <you@example.com>
2026-04-02 11:51:00 +0530

    Initial commit

--- Oops_UserDetails.py
+++ Oops_UserDetails.py
@@ -1,3 +1,4 @@
+ # Object-Oriented Programming (OOP) is a programming paradigm that uses objects and classes to structure code. In OOP, a class is a blueprint for creating objects, which are instances of the class. Each object can have attributes (data) and methods (functions) that operate on the data.
 class User:
     id = 1
     first_name = ""
@@ -16,3 +17,19 @@ print(User1.last_name)
 print(User1.email)
 User1.age = 30
 print(User1.age)
+
+# Constructor is a special method in a class that is automatically called when an object of the class is created. It is typically used to initialize the attributes of the object. In Python, the constructor method is defined using the __init__() method.
+class User:
+    def __init__(self, id, first_name, last_name, email, password):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.is_active = True
+User1 = User(1, "Dhiwagar", "S", "dhiwagar22@gmail.com", "password123")
+print(User1.id, User1.first_name.capitalize(), User1.last_name.capitalize(), User1.email, User1.password)
+User2 = User(2, "John", "Doe", "john.doe@gmail.com", "password456")
+print(User2.first_name)
+User3 = User(3, "Jane", "Smith", "jane.smith@gmail.com", "password789")
+print(User3.first_name)
